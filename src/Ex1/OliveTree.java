903034908407e274c35b51c239d7b736c41ccd4f
Elad <fisherelad3@gmail.com>
2022-02-23 16:50:42 +0200

    suicide #8

--- src/Ex1/OliveTree.java
+++ src/Ex1/OliveTree.java
@@ -6,41 +6,41 @@ public class OliveTree extends Tree {
 	boolean hasFruit;
 	
 	OliveTree(int height, Season season) {
-		super(height,season,Color.GREEN);
+		super(height, season, Color.GREEN);
 		
 		hasFruit = false;
-		if(season==Season.FALL)
+		if (season == Season.FALL)
 			hasFruit = true;
 	}
 	
 	@Override
 	public String toString() {
-		String res = "Olive tree. I give fruit. "+super.toString();
+		String res = "Olive tree. I give fruit. " + super.toString();
 		
 		return res;
 	}
+	
 	@Override
 	public void changeSeason() {
 		super.changeSeason();
 		
 		hasFruit = false;
-		switch (season){
-			case WINTER -> {
-				this.height+=5;
+		switch (season)
+		{
+			case WINTER:
+				this.height += 5;
 				break;
-			}
 			
-			case SUMMER, SPRING -> {
-				this.height+=10;
+			
+			case SUMMER, SPRING:
+				this.height += 10;
 				break;
-			}
 			
-			case FALL -> {
+			
+			case FALL:
 				hasFruit = true;
 				this.height += 5;
 				break;
-			}
-			
 			
 			
 		}
