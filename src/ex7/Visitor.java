0b41a9a26d58aba88ab8af4c363391955653620c
Elad <fisherelad3@gmail.com>
2022-05-18 15:20:52 +0300

    Visitor

---
+++ src/ex7/Visitor.java
@@ -0,0 +1,10 @@
+package ex7;
+
+public interface Visitor {
+     void visit(JpgFileDetails file);
+     void visit(HtmlFileDetails file);
+     void visit(Mp3FileDetails file );
+     void visit(PptxFileDetails file);
+     void visit(TxtFileDetails file);
+     void visit(DocxFileDetails file);
+}
