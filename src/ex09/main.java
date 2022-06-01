d554fa36466918c04b76fd80e038a23ac0e58617
Elad <fisherelad3@gmail.com>
2022-06-01 15:07:01 +0300

    packges

---
+++ src/ex09/main.java
@@ -0,0 +1,12 @@
+package ex09;
+
+public static void main(String[] args)
+        {
+        WeatherMonitoringSystem ws =
+        WeatherMonitoringSystem.theInstance();
+
+        MonitoringScreen p_MonitorScreen =
+        new MonitoringScreen();
+        Log p_log = new Log();
+
+        }
